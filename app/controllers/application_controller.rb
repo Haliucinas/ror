1629cc78ac424ac0faf281d75048eba50fb9dd91
Haliucinas <mantas@jonytis.eu>
2016-01-09 23:57:23 +0200

    Add a Static Pages controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    render :text => env['action_dispatch.remote_ip'].calculate_ip
+  end
 end
